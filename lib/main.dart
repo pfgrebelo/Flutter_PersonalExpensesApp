99d4d9c1752c1637a6d009e8a1399811fa6c1137
Pedro Rebelo <pfiliperebelo@gmail.com>
2022-11-29 12:35:53 +0000

    improved transaction row layout

--- lib/main.dart
+++ lib/main.dart
@@ -65,19 +65,36 @@ class MyHomePage extends StatelessWidget {
                             ),
                             decoration: BoxDecoration(
                               border: Border.all(
-                                color: Colors.black,
+                                color: Colors.purple,
                                 width: 2,
                               ),
                             ),
                             padding: EdgeInsets.all(10),
                             child: Text(
                               tx.amount.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
                             ),
                           ),
                           Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(tx.title.toString()),
-                              Text(tx.date.toString())
+                              Text(
+                                tx.title.toString(),
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              Text(
+                                tx.date.toString(),
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              )
                             ],
                           )
                         ],
