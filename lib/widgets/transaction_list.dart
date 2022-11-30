39b962c950889f921e74fa0c3fad74a1044a46ca
Pedro Rebelo <pfiliperebelo@gmail.com>
2022-11-30 15:51:28 +0000

    small update

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -14,7 +14,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 450,
+      height: 520,
       child: transactions.isEmpty
           ? Column(
               children: [
